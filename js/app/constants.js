9cdb752a290412565dc42e3bb24a378a0e049726
web-intelligence <info@webintelligence.ie>
2014-04-03 12:34:46 +0100

    first commit

---
+++ js/app/constants.js
@@ -0,0 +1,22 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+    var project_title = "davis", 
+        project_number = "xxx", 
+        push_server_url = "http://push.schoolspace.ie", 
+        standard_device_id = "63843", 
+        standard_api_key = "hv7Vgd4jsbb",
+        school_name = "David College Mallow",
+        twitter_handle = "cbsthurles",
+        gmail = 'daviscollegemallow@gmail.com',
+        is_push = false,
+        is_emulator = false,
+        in_browser = true;
+
+    /********device id and api key for browser testing*********/
+    var test_device_id = 87,
+        test_api_key = 'mlQnpa9c';
+  
+
